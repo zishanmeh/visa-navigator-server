3964ca643fc4ab0bedde4b6c2447a5fe6505cceb
zishanmeh <zishanmehedihasan2@gmail.com>
2024-12-04 00:43:17 +0000

    Set up server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(port);

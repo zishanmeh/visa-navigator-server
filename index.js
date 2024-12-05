97702d57e58f3269f65f410ca40a49607b151887
zishanmeh <zishanmehedihasan2@gmail.com>
2024-12-05 03:13:46 +0000

    Added post method for apply visa and store data in mongoDB

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 // J9QcTe7yYctk42R7
 // zishanmehedihasan
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.lkx4f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,10 +28,20 @@ async function run() {
     await client.connect();
 
     const visaCollection = client.db("visasDB").collection("visa");
+    const appliedVisaCollection = client
+      .db("visasDB")
+      .collection("AppliedVisaCollection");
     // Get method
     app.get("/allVisa", async (req, res) => {
       const cursor = visaCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/visa/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await visaCollection.findOne(query);
       res.send(result);
     });
 
@@ -40,6 +50,12 @@ async function run() {
     app.post("/addVisa", async (req, res) => {
       const newVisa = req.body;
       const result = await visaCollection.insertOne(newVisa);
+      console.log(newVisa);
+      res.send(result);
+    });
+    app.post("/applyVisa", async (req, res) => {
+      const newVisa = req.body;
+      const result = await appliedVisaCollection.insertOne(newVisa);
       console.log(newVisa);
       res.send(result);
     });
